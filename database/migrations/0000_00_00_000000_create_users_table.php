c6359451f987ca82893ab6870a3097e6ec63d6fe
John Wilson <john@zealouscreations.com>
2023-04-01 14:58:21 -0500

    Rename migrations so they show up at the top. Remove default in creation of users.roles json column as that is not supported in all database types. Set the User roles when saving if roles not set.

--- database/migrations/0000_00_00_000000_create_users_table.php
+++ database/migrations/0000_00_00_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration {
 			$table->timestamp( 'email_verified_at' )->nullable();
 			$table->string( 'password' );
 			$table->rememberToken();
-			$table->json( 'roles' )->default( json_encode( [] ) );
+			$table->json( 'roles' );
 		} );
 	}
 
